dac00d0fc0d05abbfb0c674546dbc4b07a3cc793
raselmia63 <147645661+raselmia63@users.noreply.github.com>
2025-03-02 17:18:05 +0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-const completeBtn = document.querySelectorAll('completed-btn-1');
+const completeBtn = document.querySelectorAll('.completed-btn-1');
 
 //ref to blog page
 document.getElementById('discover-section').addEventListener('click', function(){
